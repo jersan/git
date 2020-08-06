f4517675fff958f2ec4f30f46c07c0d47ecc407c
jersan <912396375@qq.com>
2020-08-06 17:37:56 +0800

    hello github

---
+++ src/main/java/com/offcn/git/demo/hello.java
@@ -0,0 +1,13 @@
+package com.offcn.git.demo;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+
+}
